e5172da78ddfc25bed4425114d5fd3ceef7a4b24
Diego Chiodini <diego.chiodini@esmenezes.com.br>
2024-11-21 19:23:43 -0300

    Create useCase createRole

--- src/roles/http/routes/roules.routes.ts
+++ src/roles/http/routes/roules.routes.ts
@@ -1,34 +1,18 @@
+import { Router } from 'express';
+import { createRolesController } from '@roles/useCases/createRole';
 import { RolesRepository } from '@roles/repositories/RolesRepository';
-import { Request, Response, Router } from 'express';
 
 const rolesRouter = Router();
 const rolesRepository = new RolesRepository();
 
-rolesRouter.post('/', (request: Request, response: Response) => {
-  const { name } = request.body;
-  const roleAlreadyExists = rolesRepository.findByName(name);
-
-  if (roleAlreadyExists) {
-    return response.status(400).json({
-      error: 'Role already exists',
-    });
-  }
-
-  const role = rolesRepository.create({ name });
-
-  return response.status(201).json(role);
+rolesRouter.post('/', (request, response) => {
+  return createRolesController.handle(request, response);
 });
 
-rolesRouter.get('/', (request: Request, response: Response) => {
+rolesRouter.get('/', (request, response) => {
   const roles = rolesRepository.findAll();
 
   return response.status(200).json(roles);
 });
-
-// rolesRouter.get('/', (request: Request, response: Response) => {
-//   const roles = rolesRepository.findAll();
-
-//   return response.status(200).json(roles);
-// });
 
 export { rolesRouter };
