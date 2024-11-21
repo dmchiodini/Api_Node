fb995eab1f144397de37459def51cdb7884ff273
Diego Chiodini <diego.chiodini@esmenezes.com.br>
2024-11-21 19:36:50 -0300

    Config singleton in rolesRepository

--- src/roles/http/routes/roules.routes.ts
+++ src/roles/http/routes/roules.routes.ts
@@ -1,10 +1,8 @@
 import { Router } from 'express';
 import { createRolesController } from '@roles/useCases/createRole';
-import { RolesRepository } from '@roles/repositories/RolesRepository';
 import { listRolesController } from '@roles/useCases/listRoles';
 
 const rolesRouter = Router();
-const rolesRepository = new RolesRepository();
 
 rolesRouter.post('/', (request, response) => {
   return createRolesController.handle(request, response);
