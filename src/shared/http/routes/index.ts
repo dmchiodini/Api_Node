b279a27aab42dfe4370a124ac7a9655bb41dca71
Diego Chiodini <diego.chiodini@esmenezes.com.br>
2024-11-21 14:40:25 -0300

    Add error middleware

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,8 +1,9 @@
-import { Router } from 'express';
+import { AppError } from '@shared/errors/AppError';
+import { Router, Request, Response } from 'express';
 
 const routes = Router();
 
-routes.get('/', (request, response) => {
+routes.get('/', (request: Request, response: Response) => {
   return response.json({
     message: 'Olá Dev',
   });
