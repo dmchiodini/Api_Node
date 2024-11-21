3e55f9f8bf393ea998a9bac0971b8afdf9ea9d21
Diego Chiodini <diego.chiodini@esmenezes.com.br>
2024-11-21 16:39:52 -0300

    Add role entity

--- src/roles/http/routes/roules.routes.ts
+++ src/roles/http/routes/roules.routes.ts
@@ -1,17 +1,15 @@
 import { Request, Response, Router } from 'express';
-import { v4 as uuidv4 } from 'uuid';
-
+import { Role } from '@roles/entities/Role';
 const rolesRouter = Router();
 
-const roles = [];
+const roles: Role[] = [];
 
 rolesRouter.post('/', (request: Request, response: Response) => {
   const { name } = request.body;
-  const role = {
-    id: uuidv4(),
-    name,
-    created_at: new Date(),
-  };
+
+  const role = new Role();
+
+  Object.assign(role, { name, created_at: new Date() });
 
   roles.push(role);
 
