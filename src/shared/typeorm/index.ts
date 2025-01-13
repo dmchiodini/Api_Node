da080ced361fab50c5050a5a840659f36dfbd0a9
Diego Chiodini <diego.chiodini@esmenezes.com.br>
2025-01-13 10:59:34 -0300

    Create migration CreateRolesTable

--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -1,8 +1,9 @@
 import { DataSource } from 'typeorm';
+import { CreateRolesTable1736776129253 } from './migrations/1736776129253-CreateRolesTable';
 
 export const dataSource = new DataSource({
   type: 'sqlite',
   database: './db.sqlite',
   entities: [],
-  migrations: [],
+  migrations: [CreateRolesTable1736776129253],
 });
