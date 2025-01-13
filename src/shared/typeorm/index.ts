f3188df5b8cb9bfe78f2817f09fd93b511dd8854
Diego Chiodini <diego.chiodini@esmenezes.com.br>
2025-01-13 11:16:37 -0300

    Refactor Role entity

--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -1,9 +1,10 @@
 import { DataSource } from 'typeorm';
 import { CreateRolesTable1736776129253 } from './migrations/1736776129253-CreateRolesTable';
+import { Role } from '@roles/entities/Role';
 
 export const dataSource = new DataSource({
   type: 'sqlite',
   database: './db.sqlite',
-  entities: [],
+  entities: [Role],
   migrations: [CreateRolesTable1736776129253],
 });
