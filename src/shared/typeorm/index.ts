2e1695de66854f8dae8c6f1b20dead7395dd5980
Diego Chiodini <diego.chiodini@esmenezes.com.br>
2025-01-16 15:51:10 -0300

    Create user entity

--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -3,11 +3,12 @@ import { CreateRolesTable1736776129253 } from './migrations/1736776129253-Create
 import { Role } from '@roles/entities/Role';
 import { CreateUsersTable1737049571364 } from './migrations/1737049571364-CreateUsersTable';
 import { AddRoleIdToUsersTable1737050186433 } from './migrations/1737050186433-AddRoleIdToUsersTable';
+import { User } from '@users/entities/User';
 
 export const dataSource = new DataSource({
   type: 'sqlite',
   database: './db.sqlite',
-  entities: [Role],
+  entities: [Role, User],
   migrations: [
     CreateRolesTable1736776129253,
     CreateUsersTable1737049571364,
