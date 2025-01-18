f9bfbc3ecf2655fd9fc5d1594cdbfa2d128d1ab5
Diego Chiodini <diego.chiodini@esmenezes.com.br>
2025-01-18 16:58:29 -0300

    Create showProfileRoute

--- src/users/http/routes/users.routes.ts
+++ src/users/http/routes/users.routes.ts
@@ -8,12 +8,15 @@ import { CreateLoginrController } from '@users/useCases/createLogin/CreateLoginC
 import { isAuthenticated } from '@shared/http/middlewares/isAuthenticated';
 import uploadConfig from '@config/upload';
 import { UpdateAvatarController } from '@users/useCases/updateAvatar/UpdateAvatarController';
+import { ShowProfileController } from '@users/useCases/showProfile/ShowProfileController';
 
 const usersRouter = Router();
 const createUserController = container.resolve(CreateUserController);
 const listUsersController = container.resolve(ListUsersController);
 const createLoginController = container.resolve(CreateLoginrController);
 const updateAvatarController = container.resolve(UpdateAvatarController);
+const showProfileController = container.resolve(ShowProfileController);
+
 const upload = multer(uploadConfig);
 
 usersRouter.post(
@@ -68,5 +71,9 @@ usersRouter.patch(
     return updateAvatarController.handle(request, response);
   },
 );
+
+usersRouter.get('/profile', isAuthenticated, (request, response) => {
+  return showProfileController.handle(request, response);
+});
 
 export { usersRouter };
