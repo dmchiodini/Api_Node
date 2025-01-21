e47371aea9b644354396cb0f361a50e4dc9d6800
Diego Chiodini <diego.chiodini@esmenezes.com.br>
2025-01-21 18:37:22 -0300

    Create migration CreateRefreshTokensTable

--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -4,6 +4,7 @@ import { Role } from '@roles/entities/Role';
 import { CreateUsersTable1737049571364 } from './migrations/1737049571364-CreateUsersTable';
 import { AddRoleIdToUsersTable1737050186433 } from './migrations/1737050186433-AddRoleIdToUsersTable';
 import { User } from '@users/entities/User';
+import { CreateRefreshTokenTable1737494963781 } from './migrations/1737494963781-CreateRefreshTokenTable';
 
 export const dataSource = new DataSource({
   type: 'sqlite',
@@ -13,5 +14,6 @@ export const dataSource = new DataSource({
     CreateRolesTable1736776129253,
     CreateUsersTable1737049571364,
     AddRoleIdToUsersTable1737050186433,
+    CreateRefreshTokenTable1737494963781,
   ],
 });
